b039df3d30cfae31f17072836ecc12e9c82ec653
dy1owens <dylowens@gmail.com>
2023-05-20 13:59:43 -0700

    Update wsgi.py

--- DjangoApp/wsgi.py
+++ DjangoApp/wsgi.py
@@ -6,10 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+"what is this?"
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoApp.settings')
 
